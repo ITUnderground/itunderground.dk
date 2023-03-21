babe0255b88699aeae78b1abbe82d34764ccdcbd
Alexander Thoren <thoren.alex@gmail.com>
2023-03-21 03:43:17 +0100

    ✨ Added touch command

--- src/lib/shell/commands/index.ts
+++ src/lib/shell/commands/index.ts
@@ -8,3 +8,4 @@ export { default as ls } from './ls';
 export { default as cat } from './cat';
 export { default as cd } from './cd';
 export { default as history } from './history';
+export { default as touch } from './touch';
