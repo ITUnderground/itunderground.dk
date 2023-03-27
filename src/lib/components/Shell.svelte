98842f0895da3230b7e40c39b0551a744cce45e5
Alexander Thoren <thoren.alex@gmail.com>
2023-03-27 22:45:12 +0200

    ✨ Added left/right arrow navigation

--- src/lib/components/Shell.svelte
+++ src/lib/components/Shell.svelte
@@ -12,56 +12,72 @@
 	let interactive = prerun.length === 0; // Whether the user can interact with the shell
 
 	let input = '';
+	let input_right = '';
 	$: log = [...cli.log];
 	$: cwd = cli.dir.cwd.replace('/home/itunderground', '~');
 
 	function submit() {
 		if (!interactive) return;
+		input += input_right;
+		input_right = '';
 		cli.run(input);
 		input = '';
 		log = [...cli.log];
 		cwd = cli.dir.cwd.replace('/home/itunderground', '~');
-        historyIndex = cli.history.length;
+		historyIndex = cli.history.length;
 	}
 
-    // Navigate history
-    let historyIndex = cli.history.length - 1;
-    /** @type {(direction: string) => void} */
-    function navigateHistory(direction) {
-        if (!interactive) return;
-        if (direction === 'up') {
-            if (historyIndex > 0) {
-                historyIndex--;
-                input = cli.history[historyIndex];
-            }
-        } else if (direction === 'down') {
-            if (historyIndex < cli.history.length - 1) {
-                historyIndex++;
-                input = cli.history[historyIndex];
-            } else {
-                historyIndex = cli.history.length;
-                input = '';
-            }
-        }
-    }
+	// Navigate history
+	let historyIndex = cli.history.length - 1;
+	/** @type {(direction: string) => void} */
+	function navigateHistory(direction) {
+		if (!interactive) return;
+		if (direction === 'up') {
+			if (historyIndex > 0) {
+				historyIndex--;
+				input = cli.history[historyIndex];
+			}
+		} else if (direction === 'down') {
+			if (historyIndex < cli.history.length - 1) {
+				historyIndex++;
+				input = cli.history[historyIndex];
+			} else {
+				historyIndex = cli.history.length;
+				input = '';
+			}
+		}
+	}
 
 	/**
 	 * @param e {KeyboardEvent & { currentTarget: EventTarget & Window; }}
 	 */
 	function onKeyDown(e) {
 		if (!interactive) return;
-        // Navigate history
-        if (e.key === 'ArrowUp') {
-            navigateHistory('up');
-        } else if (e.key === 'ArrowDown') {
-            navigateHistory('down');
-        }
+		// Navigate history
+		if (e.key === 'ArrowUp') {
+			navigateHistory('up');
+		} else if (e.key === 'ArrowDown') {
+			navigateHistory('down');
+		}
+
+		// Navigate text left/right
+		if (e.key === 'ArrowLeft') {
+			input_right = input.slice(-1) + input_right;
+			input = input.slice(0, -1);
+		} else if (e.key === 'ArrowRight') {
+			input = input + input_right.slice(0, 1);
+			input_right = input_right.slice(1);
+		}
+
 		// Add key to input
 		if (e.key.length === 1 && !(e.ctrlKey || e.altKey || e.metaKey)) {
 			input += e.key;
 		}
 		if (e.key === 'Backspace') {
 			input = input.slice(0, -1);
+		}
+		if (e.key === 'Delete') {
+			input_right = input_right.slice(1);
 		}
 		// Submit on enter
 		if (e.key === 'Enter') {
@@ -109,8 +125,8 @@
 			// Update log and cwd
 			log = [...cli.log];
 			cwd = cli.dir.cwd.replace('/home/itunderground', '~');
-            // Update history index
-            historyIndex = cli.history.length;
+			// Update history index
+			historyIndex = cli.history.length;
 			// Wait for next line
 			if (animationSpeed.lines !== 0)
 				await new Promise((resolve) => setTimeout(resolve, animationSpeed.lines));
@@ -121,12 +137,12 @@
 	type();
 </script>
 
-<div class="w-full text-lg leading-5">
+<div class="w-full">
 	{#each log as line}
 		{#if 'user' in line}
-			<span class="text-green-500"><strong>{line.user}@{line.server}</strong></span>:<span
-				class="text-pink-500"><strong>{line.cwd}</strong></span
-			>$ <span>{line.command}</span>
+			<span class="text-[var(--shellcolor-home)]"><strong>{line.user}@{line.server}</strong></span
+			>:<span class="text-[var(--shellcolor-base)]"><strong>{line.cwd}</strong></span>$
+			<span>{line.command}</span>
 		{/if}
 		{#if 'output' in line && line.output !== ''}
 			<br />
@@ -136,11 +152,12 @@
 		{/if}
 		<br />
 	{/each}
-	<span class="text-green-500"
+	<span class="text-[var(--shellcolor-home)]"
 		><strong>{CLI.commands.whoami()}@{CLI.commands.hostname()}</strong></span
-	>:<span class="text-pink-500"><strong>{cwd}</strong></span>$ <span>{input}</span><span
-		class="cursor"
-	/>
+	>:<span class="text-[var(--shellcolor-base)]"><strong>{cwd}</strong></span>$
+	<span>{input}</span><span class="cursor" /><span
+		class="-ml-[0.8rem] text-[var(--shellcolor-base)]">{input_right}</span
+	>
 </div>
 <svelte:window on:keydown={onKeyDown} />
 
@@ -151,7 +168,7 @@
 	}
 
 	:global(a) {
-		@apply underline;
+		@apply text-[#9cbbc8] underline;
 	}
 	:global(a:hover) {
 		@apply text-sky-500 no-underline;
@@ -159,9 +176,12 @@
 
 	:global(body) {
 		--purple: #dba9ff66;
+		--shellcolor-home: #4beb53;
+		--shellcolor-path: #d85aa5;
+		--shellcolor-base: #c8c1b4;
 	}
 	.cursor {
-		@apply inline-block h-[1.2rem] w-[0.6rem];
+		@apply inline-block h-[1.5rem] w-[0.8rem];
 		@apply align-text-bottom;
 		@apply bg-purple-500;
 		animation: blink 1s infinite;
