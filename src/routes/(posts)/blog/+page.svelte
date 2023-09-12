7a8e6dcf841cd28acacfb6c5a5bf4e733b7814da
TheColorman <thoren.alex@gmail.com>
2023-09-12 19:14:52 +0200

    ✨ Add first blog post
    Getting set up with Kali Linux on Windows (WSL 2)

---
+++ src/routes/(posts)/blog/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import { onMount } from 'svelte';
+
+	// Redirect to / without adding to history
+	// This page only exists for the breadcrumbs
+	onMount(() => window.location.replace('/?command=cat%20blog-posts'));
+</script>
