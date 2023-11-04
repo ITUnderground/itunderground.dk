4108b44ca28ba7b45598095a246b1c5fb3bb9f60
TheColorman <thoren.alex@gmail.com>
2023-11-05 00:38:37 +0100

    :sparkles: Made filesystem persistent

--- src/lib/shell/dir.ts
+++ src/lib/shell/dir.ts
@@ -1,34 +1,10 @@
 import type Env from './env';
+import { dir as dirStore, dirDefault } from '$lib/stores';
 import type { Directory, File } from './types';
+import { get } from 'svelte/store';
 // Implements directory structure
 class Dir {
-	private _root: Directory = {
-		home: {
-			// Folder
-			itunderground: {
-				// Simple file
-				'flag.txt': "Did you really think it'd be that easy?",
-				secret_folder: {
-					'inconspicuous.txt': "You found me! Here's your flag: <code>itu{spooky}</code>"
-				},
-				// Multiline file
-				underground:
-					'├── <a href="/pages/who-are-we">who-are-we</a>\n' +
-					'├── <a href="/pages/next-events">next-events</a>\n' +
-					'├── <a href="/pages/discord">discord-server</a>\n' +
-					'├── <a href="/pages/resources">resources</a>\n' +
-					'├── <a href="/?command=cat%20blog-posts">blog-posts/</a>\n' +
-					'└── <a href="/?command=cat%20writeups">writeups/</a>',
-				'blog-posts':
-					'└── <a href="/blog/setting-up-kali-windows">Setting up Kali Linux on Windows</a>',
-				writeups:
-					'└── <a href="/blog/writeups/fectf23">FE CTF 2023 - The UniPwnie Experience</a>\n' +
-					'    ├── <a href="/blog/writeups/fectf23/admin-cli">Admin CLI</a>\n' +
-					'    ├── <a href="/blog/writeups/fectf23/inception">Inception</a>\n' +
-					'    └── <a href="/blog/writeups/fectf23/padding-oracle">Padding Oracle</a>'
-							}
-		}
-	};
+	private _root: Directory = get(dirStore);
 	private _cwd: string[];
 	private _current = this._root;
 	private _env: Env;
