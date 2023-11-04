133eac6ee65c44c934113fa10f27bf27f9173b37
TheColorman <thoren.alex@gmail.com>
2023-11-04 22:53:42 +0100

    :bug: Fix sudo

--- src/lib/shell/commands/sudo.ts
+++ src/lib/shell/commands/sudo.ts
@@ -1,11 +1,11 @@
 import Command from '../command';
 
 export default new Command({
-	command({ command: { raw }, cli }) {
+	async command({ command: { raw }, cli }) {
 		const subcommand = raw.split(' ').slice(1).join(' ');
 		if (!subcommand) return 'sudo: missing command';
 		const parsed = cli._argParser(subcommand);
-		const output = cli._execute(parsed);
+		const output = await cli._execute(parsed);
 		return `<span style="color: red;">Running ${subcommand} in G A M E R M O D E: </span>\n${output}`;
 	},
 	description: 'Runs a command in G A M E R M O D E',
