8b507f51f994149b42aaba1074c0735bd195d07f
TheColorman <thoren.alex@gmail.com>
2023-11-06 04:15:45 +0100

    ✏️ Add clarification to recover command

--- src/lib/shell/commands/recoverfilesystembecauseideleteditanddidntthinkoftheconsequences.ts
+++ src/lib/shell/commands/recoverfilesystembecauseideleteditanddidntthinkoftheconsequences.ts
@@ -7,7 +7,8 @@ export default new Command({
 		return (
 			'<span style="color: red">\n' +
 			'wow you really did it. you really deleted the filesystem.\n' +
-			'you really thought that was a good idea.</span>'
+			'you really thought that was a good idea.</span>\n' +
+			"don't worry. I fixed it for you."
 		);
 	},
 	description: 'Resets the filesystem in case you did something stupid',
