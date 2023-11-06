31537b4386befa0d2e1fe1f99460aef17caabfb4
TheColorman <thoren.alex@gmail.com>
2023-11-06 19:17:30 +0100

    :sparkles: Add linu number support to cat

--- src/lib/shell/commands/cat.ts
+++ src/lib/shell/commands/cat.ts
@@ -1,9 +1,24 @@
 import Command from '../command';
 
+const help = `Usage: cat [OPTION]... [FILE]...
+Concatenate FILE(s) to standard output.
+
+With no FILE, or when FILE is -, read standard input.
+
+  -n, --number             number all output lines
+      --help        display this help and exit
+
+Examples:
+  cat        Copy standard input to standard output.
+
+GNU coreutils online help: <https://www.gnu.org/software/coreutils/>
+Full documentation <https://www.gnu.org/software/coreutils/cat>
+or available locally via: info '(coreutils) cat invocation'`;
+
 export default new Command({
-	command({ command: { positionalArguments: positional }, dir }) {
+	command({ command: { positionalArguments, namedArguments }, dir }) {
 		// Get file parameter
-		const requestedFile = positional[0];
+		const requestedFile = positionalArguments[0];
 		if (!requestedFile) return 'cat: missing file operand';
 
 		// Get file
@@ -11,8 +26,25 @@ export default new Command({
 		if (!file) return `cat: ${requestedFile}: No such file or directory`;
 		if (file.type === 'Directory') return `cat: ${requestedFile}: Is a directory`;
 
-		return typeof file.value === 'string' ? file.value : '';
+		return (() => {
+			if (typeof file.value !== 'string') return '';
+
+			const content = file.value.split('\n');
+			if (namedArguments['number']) {
+				return content.map((line, index) => `${index + 1} ${line}`).join('\n');
+			}
+			return content.join('\n');
+		})();
 	},
 	description: 'Concatenate files and print on the standard output.',
-	namedArguments: []
+	namedArguments: [
+		{
+			name: 'help',
+			choices: ['h', 'help']
+		},
+		{
+			name: 'number',
+			choices: ['n', 'number']
+		}
+	]
 });
