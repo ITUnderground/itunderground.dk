fd54d72aa295f427b45c0af52a88afb94d479fbb
TheColorman <thoren.alex@gmail.com>
2023-11-06 04:15:23 +0100

    ✨ Add --all flag to ls

--- src/lib/shell/commands/ls.ts
+++ src/lib/shell/commands/ls.ts
@@ -1,9 +1,19 @@
 import Command from '../command';
 
+const help = `Usage: ls [OPTION]... [FILE]...
+List information about the FILEs (the current directory by default).
+
+Mandatory arguments to long options are mandatory for short options too.
+  -a, --all                  do not ignore entries starting with .
+  -l                         use a long listing format
+      --help     display this help and exit`;
+
 export default new Command({
-	command({ command: { positionalArguments: positional }, dir }) {
+	command({ command: { positionalArguments, namedArguments }, dir }) {
+		if (namedArguments.help) return help;
+
 		// Get path parameter
-		const requestedPath = positional[0];
+		const requestedPath = positionalArguments[0];
 
 		// Locate directory
 		const directory = requestedPath ? dir.read(requestedPath) : dir.read(dir.cwd);
@@ -13,12 +23,23 @@ export default new Command({
 		// Return list of files
 		return Object.entries(directory.value)
 			.map(([key, value]) => {
+				if (typeof value === 'string' && !namedArguments.all && key.startsWith('.')) return;
 				if (typeof value === 'string') return key;
 				if (typeof value === 'object') return `<span style="color: #ec4899">${key}/</span>`;
 				return key;
 			})
+			.sort()
 			.join('\n');
 	},
 	description: 'Lists files in current directory',
-	namedArguments: []
+	namedArguments: [
+		{
+			name: 'help',
+			choices: ['h', 'help']
+		},
+		{
+			name: 'all',
+			choices: ['a', 'all']
+		}
+	]
 });
